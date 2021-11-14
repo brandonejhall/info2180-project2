368fd39696a5cf496029d8d63b52f6a00d1fcff7
Donielle20 <donielle_hope@yahoo.com>
2021-11-14 15:06:05 -0500

    Files Added

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    document.getElementsByTagName("div")[2].addEventListener("click",home);
+};
+
+function home()
+{
+    window.location = 'https://www.google.com/';
+}
