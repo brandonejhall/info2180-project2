c2a36d8b158613e954926d20e7ee7a2c5bd0d723
brandonejhall <brandonejhall7@outlook.com>
2021-11-28 22:32:22 -0500

    add_user-clean

--- bugme.php
+++ bugme.php
@@ -1,88 +1,36 @@
 <?php
-$host = 'localhost';
-$username = 'project2_user';
-$password = 'password123';
-$dbname = 'bugme';
+    $host = 'localhost';
+    $username = 'project2_user';
+    $password = 'password123';
+    $dbname = 'bugme';
 
-// connection made to database
+    // connection made to database
 
+    if ($_SERVER['REQUEST_METHOD']=== 'POST'){
 
+        if (isset($_POST)){
+            
+            $new_user=filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
+            $new_user =explode(",",$new_user);
 
-
-if ($_SERVER['REQUEST_METHOD']=== 'POST'){
-
-    if (isset($_POST)):
-        $new_user=filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
-        $new_user =explode(",",$new_user);
-
-        $firstname= $new_user[0];
-        $lastname= $new_user[1];
-        $password= $new_user[2];
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $email= $new_user[3];
+            $firstname= $new_user[0];
+            $lastname= $new_user[1];
+            $password= $new_user[2];
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $email= $new_user[3];
         
-        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-        $statement = $conn->prepare('INSERT INTO users (firstname, lastname, pass, email)
-    VALUES (:firstname, :lastname, :pass, :email)');
-
-    $statement->execute([
-        'firstname' => $firstname,
-        'lastname' => $lastname,
-        'pass' => $hashed_password,
-        'email' => $email
-    ]);
-
-        /*$stmt = $conn->prepare("INSERT INTO users (firstname,lastname,pass,email) VALUES (:firstname,:lastname,:pass,:email)");
-        $stmt ->bindParam(':firstname',$firstname);
-        $stmt ->bindParam(':lastname',$lastname);
-        $stmt ->bindParam(':pass',$hashed_password);
-        $stmt ->bindParam(':email',$email);
+            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
         
-        
-        $conn -> execute($stmt);
+            $statement = $conn->prepare('INSERT INTO users (firstname, lastname, pass, email) VALUES (:firstname, :lastname, :pass, :email)');
+
+            $statement->execute(['firstname' => $firstname,'lastname' => $lastname,'pass' => $hashed_password,'email' => $email]);
+        };
 
 
-        $lastInsertId = $conn->lastInsertId();
-        if($lastInsertId>0)
-        {
-        echo "OK";
-        }
-        else
-        {
-        echo "not OK";
-        }/*
+    };
 
 
-
-        
-
-
-
-        /*try{
-        $new_user = filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
-        $new_user = explode(" ",$new_user);
-        
-        $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $firstname= $new_user[0];
-        $lastname= $new_user[1];
-        $password= $new_user[2];
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $email= $new_user[3];
-
-        $stmt = $conn->prepare("INSERT INTO users (firstname,lastname,password,email) VALUES ($firstname,$lastname,$hashed_password,$email)");
-        $conn -> exec($stmt);
-        }catch(PDOException $e) {
-        
-        echo $stmt . "<br>" . $e->getMessage();};
-
-        $conn = null;*/
-    endif;
-
-
-
-};
+    
 
 
 ?>
