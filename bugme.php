9da6aea7d43cfcfdd654e1b75d778bc1b8ffe889
brandonejhall <brandonejhall7@outlook.com>
2021-11-29 01:03:17 -0500

    login

--- bugme.php
+++ bugme.php
@@ -4,11 +4,13 @@
     $password = 'password123';
     $dbname = 'bugme';
 
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
     // connection made to database
 
-    if ($_SERVER['REQUEST_METHOD']=== 'POST'):
+    if($_SERVER['REQUEST_METHOD']=== 'POST'):
 
-        if (isset($_POST['add_user'])){
+        if(isset($_POST['add_user'])):
             
             $new_user=filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
             $new_user =explode(",",$new_user);
@@ -19,12 +21,38 @@
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
             $email= $new_user[3];
         
-            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+            
         
             $statement = $conn->prepare('INSERT INTO users (firstname, lastname, pass, email) VALUES (:firstname, :lastname, :pass, :email)');
 
             $statement->execute(['firstname' => $firstname,'lastname' => $lastname,'pass' => $hashed_password,'email' => $email]);
-        };
+
+        
+        endif;
+
+
+        if(isset($_POST['login'])):
+            $ret_user = filter_var($_POST['login'],FILTER_SANITIZE_STRING);
+            $ret_user = explode(",",$new_user);
+
+            $email = $ret_user[0];
+            $password = password_hash($ret_user[1]);
+
+            $statement = $conn->query("SELECT FIRST FROM users WHERE users.email == $email");
+            
+            if($statement['pass']==$password):
+                session_start();
+                $_SESSION['id'] = $statement['id'];
+                $_SESSION['firstname'] = $statement['firstname'];
+                $_SESSION['lastname'] = $statement['lastname'];
+                $_SESSION['email'] = $statement['email'];
+                echo "Session Started";
+            endif;
+
+
+        endif;
+
+
 
 
     endif;
