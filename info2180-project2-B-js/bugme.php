6f700d98c22ee01a7dfd99af24ba432916947a98
Giovanne01 <72521096+Giovanne01@users.noreply.github.com>
2021-11-29 17:20:36 -0500

    Add files via upload
    latest files

---
+++ info2180-project2-B-js/bugme.php
@@ -0,0 +1,121 @@
+<?php
+    $host = 'localhost';
+    $username = 'project2_user';
+    $password = 'password123';
+    $dbname = 'bugme';
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+    // connection made to database
+
+    if($_SERVER['REQUEST_METHOD']=== 'POST'):
+
+        if(isset($_POST['add_user'])):
+            
+            $new_user=filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
+            $new_user =explode(",",$new_user);
+
+            $firstname= $new_user[0];
+            $lastname= $new_user[1];
+            $password= $new_user[2];
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $email= $new_user[3];
+        
+            
+        
+            $statement = $conn->prepare('INSERT INTO users (firstname, lastname, pass, email) VALUES (:firstname, :lastname, :pass, :email)');
+
+            $statement->execute(['firstname' => $firstname,'lastname' => $lastname,'pass' => $hashed_password,'email' => $email]);
+            echo("success");
+        
+        endif;
+
+
+        if(isset($_POST['log_user'])):
+            $ret_user = filter_var($_POST['log_user'],FILTER_SANITIZE_STRING);
+            $ret_user = explode(",",$new_user);
+
+            echo($email = $ret_user[0]);
+            $password = password_hash($ret_user[1]);
+
+            $statement = $conn->query("SELECT FIRST FROM users WHERE users.email == $email");
+            
+            if($statement['pass']==$password):
+                session_start();
+                $_SESSION['id'] = $statement['id'];
+                $_SESSION['firstname'] = $statement['firstname'];
+                $_SESSION['lastname'] = $statement['lastname'];
+                $_SESSION['email'] = $statement['email'];
+                echo "Session Started";
+            endif;
+
+
+        endif;
+
+
+
+
+    endif;
+
+    
+
+
+    
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php /*
+foreach ($admin as $ad):
+    echo($ad['firstname']);
+endforeach;
+
+if ($_SERVER['REQUEST_METHOD']=='POST'):
+    if (isset($_POST)):
+        $new_user=filter_var($_POST['add_user'],FILTER_SANITIZE_STRING);
+        $new_user =explode(",",$new_user);
+        echo($id='');
+        echo($firstname= $new_user[0]);
+        echo($lastname= $new_user[1]);
+        echo($password= $new_user[2]);
+        echo($hashed_password = password_hash($password, PASSWORD_DEFAULT));
+        echo($email= $new_user[3]);
+        echo($date=(date("Y-m-d h:i:s")));
+        
+       // $new_u = ['id'=>'', 'firstname' => $firstname,'lastname' => $lastname,'password' => $hashed_password,'email' => $email];
+
+        //$new_admin = "INSERT INTO users (firstname, lastname, password,email) VALUES (:firstname ,:lastname,:password,:email)";
+        //$new_admin -> execute();
+    endif;
+    
+else:
+    echo("nooo");
+endif; */
+
+
+?>
---
+++ info2180-project2-B-js/script.js
@@ -0,0 +1,230 @@
+window.onload = function()
+{
+    fetch("./Login.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementsByClassName("content")[0].innerHTML = data;
+        });
+    document.getElementsByClassName("Home")[0].addEventListener("click",home);
+    document.getElementsByClassName("Add")[0].addEventListener("click",add);
+    document.getElementsByClassName("New")[0].addEventListener("click",New);
+    document.getElementsByClassName("Logout")[0].addEventListener("click",login);
+};
+
+function home()
+{
+    fetch("./home.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementsByClassName("content")[0].innerHTML = data;
+        });
+}
+
+function add()
+{
+    fetch("./user.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementsByClassName("content")[0].innerHTML = data;
+            document.getElementsByTagName("button")[0].addEventListener("click", function(element)
+                {
+                    element.preventDefault();
+                    clearErrors();
+                    //VALIDATION OF FEILDS
+                    let approve=false;    
+                    let validationFailed=false;
+                    let add_fname= document.querySelector('#fname');
+                    let add_lname= document.querySelector('#lname');
+                    let add_email= document.querySelector('#email');
+                    let add_password= document.querySelector('#password');
+
+
+                    //Check if first name is empty
+                    if (isEmpty(add_fname.value.trim())){
+                        validationFailed=true;
+                        approve=false;
+                        add_fname.style.border="2px solid red";
+                        displayErrorMessage(add_fname,"*You must fill in your First Name*");
+                    }else{
+                        add_fname.style.removeProperty("border");
+                        approve=true;
+                    }
+
+                    //Check if last name is empty
+                    if (isEmpty(add_lname.value.trim())){
+                        validationFailed=true;
+                        approve=false;
+                        add_lname.style.border="2px solid red";
+                        displayErrorMessage(add_lname,"*You must fill in your Last Name*");
+                    }else{
+                        add_lname.style.removeProperty("border");
+                        approve=true;
+                    }
+
+                    //check if password is empty
+                    if (isEmpty(add_password.value.trim())){
+                        validationFailed=true;
+                        approve=false;
+                        add_password.style.border="2px solid red";
+                        displayErrorMessage(add_password,"*You must fill in your Last Name*");
+                    }else{
+                        add_password.style.removeProperty("border");
+                        approve=true;
+                    }
+
+                    //check if email is valid
+                    if (!isValidEmail(add_email.value.trim())){
+                        validationFailed=true;
+                        approve=false;
+                        add_email.style.border="2px solid red";
+                        displayErrorMessage(add_email,"*Invalid email address*");
+                    }else{
+                        add_email.style.removeProperty("border");
+                        approve=true;
+
+                    }
+
+                    
+                    if (approve){
+                        let httpRequest = new XMLHttpRequest();
+
+                        var fname = document.getElementsByTagName("input")[0].value;
+                        var lname = document.getElementsByTagName("input")[1].value;
+                        var pass = document.getElementsByTagName("input")[2].value;
+                        var email = document.getElementsByTagName("input")[3].value;
+                        const add_user = [fname,lname,pass,email];
+
+                        var url = "bugme.php";
+
+                        httpRequest.onreadystatechange = processName;
+                        httpRequest.open('POST', url,true);
+                        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                        httpRequest.send('add_user=' + encodeURIComponent(add_user));
+
+                        function processName()
+                        {
+                            if (httpRequest.readyState === XMLHttpRequest.DONE) 
+                            {
+                                if (httpRequest.status === 200) 
+                                {
+                                    let response = httpRequest.responseText;
+                                    console.log(response);
+                                } 
+                                else 
+                                {
+                                alert('There was a problem with the request.');
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                );
+        });
+}
+
+function New()
+{
+    fetch("./New_Issue.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementsByClassName("content")[0].innerHTML = data;
+    
+        });
+}
+
+function login()
+{
+    fetch("./Login.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementsByClassName("content")[0].innerHTML = data;
+            document.getElementById('loginBtn').addEventListener("click", function(e){
+                e.preventDefault();
+                let log_pw= document.querySelector('#password');
+                let log_email= document.querySelector('#email');
+                let log_user= [log_email,log_pw];
+                var url = "bugme.php";
+                console.log("yo");
+                let httpRequest = new XMLHttpRequest();
+
+                httpRequest.onreadystatechange = processName;
+                        httpRequest.open('POST', url,true);
+                        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                        httpRequest.send('add_user=' + encodeURIComponent(log_user));
+
+                        function processName()
+                        {
+                            if (httpRequest.readyState === XMLHttpRequest.DONE) 
+                            {
+                                if (httpRequest.status === 200) 
+                                {
+                                    let response = httpRequest.responseText;
+                                    console.log(response);
+                                } 
+                                else 
+                                {
+                                alert('There was a problem with the request.');
+                                }
+                            }
+                        }
+                
+            })
+        });
+}
+
+//VALIDATION CODE
+function isEmpty(elementValue) {
+    if (elementValue.length == 0) {
+      // Or you could check if the value == ""
+      console.log('field is empty');
+      return true;
+    } 
+    return false;
+}
+
+function isValidID(id) {
+    return /^\d{9}$/.test(id);
+}
+
+function isReference(ref){
+    return /^\d{13}$/.test(ref);
+}
+
+function isValidEmail(emailAddress) {
+    return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(emailAddress);
+}
+
+function insertAfter(element, newNode) {
+    element.parentNode.insertBefore(newNode, this.nextSibling);
+}
+
+function clearErrors() {
+    var elementsWithErrors = document.querySelectorAll('.error');
+    //console.log(elementsWithErrors);
+    for (var x = 0; x < elementsWithErrors.length; x++) {
+      elementsWithErrors[x].setAttribute('class', '');
+      elementsWithErrors[x].parentNode.removeChild(elementsWithErrors[x].nextElementSibling);
+      //console.log(elementsWithErrors[x].nextElementSibling);
+    } 
+}
+
+function displayErrorMessage(formField, message) {
+    formField.setAttribute('class', 'error');
+    var errorMessageText = document.createTextNode(message);
+    var errorMessage = document.createElement('span');
+    errorMessage.setAttribute('class', 'error-message');
+    errorMessage.appendChild(errorMessageText);
+    //formField.insertAfter(errorMessage);
+    insertAfter(formField, errorMessage);
+}
